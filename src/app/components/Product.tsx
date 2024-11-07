1be1f219c23df24b53fab0b303dfbd158531c951
Candiniz <andersoncd123@gmail.com>
2024-11-07 19:23:31 -0300

    Inserção do Sistema de Login

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,5 +1,6 @@
 import { ProductType } from "@/types/ProductType"
-import Image from "next/image"
+import ProductImage from "./ProductImage"
+/*import Image from "next/image"*/
 
 type ProductProps = {
     product: ProductType
@@ -7,11 +8,14 @@ type ProductProps = {
 
 export default function Product( { product } : ProductProps ) {
     return (
-        <div className="flex flex-col shadow-lg bg-slate-800 p-3 min-h-full">
-            <div className="relative max-h-72 flex-1">
-                <Image width={300} height={300} src={product.image} alt={product.title} className="object-contain w-full h-full"  />
+        <div className="flex flex-col shadow-lg h-96 bg-slate-800 p-5">
+            <div className="relative max-h-72 flex-1"> 
+                <ProductImage product={product} fill /> 
             </div>
-            <div className="flex justify-between font-bold my-3">{product.title}</div>
+            <div className="flex justify-between font-bold my-3"> 
+                <p className="w-40 truncate">{product.title}</p> 
+                <p className="text-md text-teal-300">$ {product.price}</p>    
+            </div>
             <button className=" mt-auto rounded-md bg-teal-600 text-white p-3.5 py-2.5 tx-sm text-center">Adicionar ao Carrinho</button>
         </div>
     )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ptBR } from '@clerk/localizations'
 
 
 export const metadata: Metadata = {
@@ -14,11 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main className="bg-slate-700 min-h-screen pt-16"> {children} </main>  
-      </body>
-    </html>
+    <ClerkProvider localization={ptBR}>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main className="bg-slate-700 min-h-screen pt-16"> {children} </main>  
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
