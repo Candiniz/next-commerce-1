d1a0f137942acaa877edab6b3c07dccaecf19a9c
Candiniz <andersoncd123@gmail.com>
2024-11-07 21:47:09 -0300

    Inserindo DB com Clerk, Prisma e VercelPostgres

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <html lang="en">
         <body>
           <Navbar />
-          <main className="bg-slate-700 min-h-screen pt-16"> {children} </main>  
+          <main className="bg-slate-700 min-h-screen pt-16"> {children} </main>
         </body>
       </html>
     </ClerkProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,11 +19,11 @@ export default async function Home() {
   return (
     <div className="text-gray-300 container bg-slate-600 min-h-screen mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 lx:gap-6">
-        
+
         {products.map((product: ProductType) => (
           <Product key={product.id} product={product}></Product>
         ))}
-        
+
       </div>
     </div>
   );
